9bc1cb28144faf5ff578f3ae3e7180f57565d3c4
jmhart <jeffhart91@gmail.com>
2019-04-11 14:56:46 -0500

    Refactored notes implementation into Notes component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React, { Component } from "react";
 import Notes from "./components/Notes";
-import AddNoteButton from "./components/AddNoteButton";
-import { getNotes, addNote, deleteNote } from "./services/noteService";
 
 class App extends Component {
-  state = {
-    notes: []
-  };
-
-  componentDidMount() {
-    const notes = getNotes();
-    this.setState({ notes });
-  }
-
-  handleAddNote = () => {
-    addNote();
-    this.setState({ notes: getNotes() });
-  };
-
-  handleEditNote = (id, text) => {
-    const notes = [...this.state.notes];
-    const index = notes.findIndex(n => n.id === id);
-    notes[index].text = text;
-    this.setState({ notes });
-  };
-
-  handleDeleteNote = id => {
-    deleteNote(id);
-    this.setState({ notes: getNotes() });
-  };
-
   render() {
     return (
       <div className="App">
@@ -37,12 +9,7 @@ class App extends Component {
           <h1>Notes App</h1>
         </header>
         <main>
-          <AddNoteButton click={this.handleAddNote} />
-          <Notes
-            notes={this.state.notes}
-            handleEditNote={this.handleEditNote}
-            handleDeleteNote={this.handleDeleteNote}
-          />
+          <Notes />
         </main>
       </div>
     );
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from "react";
-import Note from "../components/Note";
+import Note from "./Note";
+import AddNoteButton from "./AddNoteButton";
+import { getNotes, addNote, deleteNote } from "../services/noteService";
 
 class Notes extends Component {
+  state = {
+    notes: []
+  };
+
+  componentDidMount() {
+    const notes = getNotes();
+    this.setState({ notes });
+  }
+
+  handleAddNote = () => {
+    addNote();
+    this.setState({ notes: getNotes() });
+  };
+
+  handleEditNote = (id, text) => {
+    const notes = [...this.state.notes];
+    const index = notes.findIndex(n => n.id === id);
+    notes[index].text = text;
+    this.setState({ notes });
+  };
+
+  handleDeleteNote = id => {
+    deleteNote(id);
+    this.setState({ notes: getNotes() });
+  };
+
   render() {
-    const { notes, handleEditNote, handleDeleteNote } = this.props;
+    const { notes } = this.state;
     return (
-      <div>
-        {notes.map(n => (
-          <Note
-            key={n.id}
-            id={n.id}
-            title={n.title}
-            text={n.text}
-            onChange={handleEditNote}
-            handleDelete={handleDeleteNote}
-          />
-        ))}
-      </div>
+      <React.Fragment>
+        <AddNoteButton click={this.handleAddNote} />
+        <div>
+          {notes.map(n => (
+            <Note
+              key={n.id}
+              id={n.id}
+              title={n.title}
+              text={n.text}
+              onChange={this.handleEditNote}
+              handleDelete={this.handleDeleteNote}
+            />
+          ))}
+        </div>
+      </React.Fragment>
     );
   }
 }
