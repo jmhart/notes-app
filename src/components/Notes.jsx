e88fcbaa094064b827413948956fdd707efad10d
Jeff Hart <jeffhart91@gmail.com>
2019-04-16 15:46:53 -0500

    Removed unused import

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Note from "./Note";
 import AddButton from "./AddButton";
-import SaveButton from "./SaveButton";
 import {
   getNotes,
   addNote,
