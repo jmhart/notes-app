92901a251f2ff82c293325947f0be296b1997061
jmhart <jeffhart91@gmail.com>
2019-05-14 14:50:30 -0500

    Added more styling to buttons and text area

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditButton = ({ disabled, onClick, isEditing }) => {
   return (
-    <button disabled={disabled} onClick={onClick}>
+    <button className="btn-edit" disabled={disabled} onClick={onClick}>
       {isEditing ? "Save note" : "Edit note"}
     </button>
   );
